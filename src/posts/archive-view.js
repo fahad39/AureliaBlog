bb169e6d77020ecb48be7269f746fb4549836ef3
fahad39 <54923717+fahad39@users.noreply.github.com>
2021-08-03 22:18:09 +0500

    added tag detail and archive detail

--- src/posts/archive-view.js
+++ src/posts/archive-view.js
@@ -1,5 +1,22 @@
+import { inject } from "aurelia-framework";
+import { PostService } from "../common/services/post-service";
+
+@inject(PostService)
 export class ArchiveView {
-  constructor() {
-    this.message = 'Hello world';
+  constructor(PostService) {
+    this.postService = PostService;
+  }
+
+  activate(params) {
+    this.archive = params.archive;
+    console.log("archiece", this.archive);
+    this.postService
+      .postsByArchive(this.archive)
+      .then((data) => {
+        this.posts = data.archive;
+      })
+      .catch((error) => {
+        this.error = error.message;
+      });
   }
 }
